08d69873dfdeb16bbf326adf2f3fa9baa6442d46
Sujan Sijapati <connectsites.sujansijapati@gmail.com>
2024-09-26 16:28:49 +0100

    commit

---
+++ Chapters/1.Variables and Datatypes/02_Datatype.py
@@ -0,0 +1,24 @@
+"""Datatype
+In Python, a data type defines the kind of value a variable can hold. Here are the main types:
+
+1.Numeric Types:
+   - int : Integer values (e.g., 5, 100)
+   - float: Floating-point numbers (e.g., 3.14, 0.001)
+
+2.Boolean Type:
+   - bool: True or False
+
+3.Text Type:
+   - str: Strings of characters (e.g., "Sujan Sijapati")
+
+Each type is crucial depending on what you're trying to do in your code!"""
+
+name = "Sujan Sijapati" #It is a string. 
+
+age = 20 #It is an Integer.
+
+pi = 3.14 #It is a Floating point number.
+
+raining = True #It is a Boolean datatype.
+
+value = None #It is a Null.
