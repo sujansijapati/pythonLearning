9ecab901f94f1f5c53c91c6dbadf4fd4e0db5ffc
Sujan Sijapati <connectsites.sujansijapati@gmail.com>
2024-09-28 15:28:27 +0100

    commit

--- Chapters/1.Variables and Datatypes/01_Variables.py
+++ Chapters/1.Variables and Datatypes/01_Variables.py
@@ -3,10 +3,10 @@ A variable in Python is like a container that stores data. It's a name you give 
 
 age = 20
 name = "Marshall"
-Here, age and name are variables."""
+Here, age and name are variables."""             
 
 num1 = int(input("Enter the number: "))
 num2 = int(input("Enter the number: "))
 
 add = num1 + num2
-print(f"The sum of {num1} and {num2} is {add}.")
+print(f"The sum of {num1} and {num2} is {add}.") 
