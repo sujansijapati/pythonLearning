0091bdc1c7dd5608e746e0f3cd6ac32dbb96317f
Sujan Sijapati <connectsites.sujansijapati@gmail.com>
2024-09-29 14:03:43 +0100

    commit

--- Chapters/2.Operators/04_Logical.py
+++ Chapters/2.Operators/04_Logical.py
@@ -11,22 +11,36 @@
    - Reverses the result: True becomes False, and False becomes True.
    - Example: not (5 > 10) results in True because (5 > 10) is False, and not makes it True.
 """
+#Truth table of "and" operator
+# Both conditions need to be true
+print("True and True is: ",True and True)
+print("True and False is: ",True and False)
+print("False and True is: ",False and True)
+print("False and False is: ",False and False)
+
+#Truth table of "or" operator
+# At least one condition needs to be true
+print("True or True is: ",True or True)
+print("True or False is: ",True or False)
+print("False or True is: ",False or True)
+print("False or False is: ",False or False)
+
+#Truth table of "NOT" operator
+# Reverse the result of the condition
+print("True not True is: ", not(True))
+
 
 a = 10
 b = 5
 c = 15
-
 # AND operator
-# Both conditions need to be true
 result_and = (a > b) and (c > a)
 print("(a > b) and (c > a):", result_and)
 
 # OR operator
-# At least one condition needs to be true
 result_or = (a < b) or (c > b)
 print("(a < b) or (c > b):", result_or) 
 
 # NOT operator
-# Reverse the result of the condition
 result_not = not (a < b)
 print("not (a < b):", result_not)
